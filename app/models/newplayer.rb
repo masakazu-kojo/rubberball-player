a2731c315762d2cd16841166c5021e59a60a663e
masakazu-kojo <2015ambition1112@gmail.com>
2020-06-10 05:32:36 +0900

    一部変更

--- app/models/newplayer.rb
+++ app/models/newplayer.rb
@@ -1,6 +1,9 @@
 class Newplayer < ApplicationRecord
   validates :content, {presence: true, length: {maximum: 140}}
   validates :user_id, {presence: true}
+  belongs_to :user
+  has_many :likes
+  has_many :liked_users, through: :likes, source: :user
 
   def user
     return User.find_by(id: self.user_id)
