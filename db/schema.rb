ae4115993b4f7f12466a7830935e25df5573e0a5
masakazu-kojo <2015ambition1112@gmail.com>
2020-06-10 21:38:50 +0900

    mysqlへ変更

--- db/schema.rb
+++ db/schema.rb
@@ -12,17 +12,14 @@
 
 ActiveRecord::Schema.define(version: 2020_05_24_082527) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "likes", force: :cascade do |t|
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "user_id"
     t.integer "newplayer_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "newplayers", force: :cascade do |t|
+  create_table "newplayers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -30,13 +27,13 @@ ActiveRecord::Schema.define(version: 2020_05_24_082527) do
     t.string "img"
   end
 
-  create_table "players", force: :cascade do |t|
+  create_table "players", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.string "email"
     t.datetime "created_at", null: false
