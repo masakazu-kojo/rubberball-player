558d024ffb76297ddf5e819cbc0c9f2f8b80846b
masakazu-kojo <2015ambition1112@gmail.com>
2020-06-15 02:48:10 +0900

    一部修正

--- app/models/newplayer.rb
+++ app/models/newplayer.rb
@@ -1,12 +1,11 @@
 class Newplayer < ApplicationRecord
+  mount_uploader :img, ImgUploader
   validates :content, {presence: true, length: {maximum: 140}}
   validates :user_id, {presence: true}
   belongs_to :user
-  has_many :likes
   has_many :liked_users, through: :likes, source: :user
 
   def user
     return User.find_by(id: self.user_id)
   end
-
 end
