843d6cde80f0fda3f2336fc790378371c8b333f5
masakazu-kojo <2015ambition1112@gmail.com>
2020-07-20 20:46:48 +0900

    js削除

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,6 +12,7 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require rails-ujs
 //= require activestorage
 //= require turbolinks
 //= require_tree .
