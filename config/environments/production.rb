6f698e3e3412098ea26fe95e8b747f3fbf07870f
masakazu-kojo <2015ambition1112@gmail.com>
2020-07-20 21:33:37 +0900

    rbの修正

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,6 +91,4 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.assets.compile = true
-  config.assets.initialize_on_precompile=false
 end
